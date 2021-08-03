8321f6a51886b5c10e0303a5cf0551f192bc1d82
20013961 <20013961@DESKTOP-C1ODDQL>
2021-08-04 07:14:10 +0800

    Jarel's test study

--- C206_CaseStudy/test/C206_CaseStudyTest.java
+++ C206_CaseStudy/test/C206_CaseStudyTest.java
@@ -1,4 +1,6 @@
 import static org.junit.Assert.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,6 +10,9 @@ public class C206_CaseStudyTest {
   private bidInfo bid1;
   private bidInfo bid2;
   private ArrayList <bidInfo> BidList;
+  private DealInfo deal1;
+  private DealInfo deal2;
+  private ArrayList <DealInfo> DealList;
   
   public C206_CaseStudyTest() {
       super();
@@ -15,12 +20,21 @@ public class C206_CaseStudyTest {
 
   @Before
   public void setUp() throws Exception {
+	  DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	  
     bid1 = new bidInfo(1, "Used Condom", "abc@gmail.com", "cadmusChau@gamil.com", 
         4.50);
     bid2 = (new bidInfo(2, "Used socks", "sccc@gmail.com", "DogeKing@gamil.com", 
         4.50));
     
-    BidList = new ArrayList<bidInfo>();        
+    BidList = new ArrayList<bidInfo>();    
+    
+    deal1 = new DealInfo(1, "Used Condom", "abc@gmail.com", "cadmusChau@gamil.com", 
+            4.50, LocalDate.parse("17/10/2020", formatter1));
+    deal2 = new DealInfo(2, "Used socks", "sccc@gmail.com", "DogeKing@gamil.com", 
+            4.50, LocalDate.parse("18/10/2020", formatter1));
+    
+    DealList = new ArrayList<DealInfo>();
   }
 
   
@@ -62,14 +76,51 @@ public class C206_CaseStudyTest {
     assertEquals("Test if that Bid arraylist size is 1?", 1, BidList.size());
     BidList.add(bid2);
     assertEquals("Test if that Bid arraylist size is 2?", 2, BidList.size());
-    
-    
+   
   }
+   @Test
+  public void ShowAllDealTest() {
+	    // Test if Item list is not null but empty, so that can add a new item
+	    assertNotNull("Test if there is valid deal arraylist to add to", DealList);
+	    
+	    DealList.add(deal1);
+	    DealList.add(deal2);
+	    assertEquals("Test if that deal arraylist size is 2?", 2, DealList.size());  
+	  }
+	  
+	  @Test
+	  public void DeleteDealTest() {
+	    // Test if Item list is not null but empty, so that can add a new item
+	    assertNotNull("Test if there is valid deal arraylist to add to", DealList);
+	    
+	    DealList.add(deal1);
+	    DealList.add(deal2);
+	    assertEquals("Test if that deal arraylist size is 2?", 2, DealList.size());
+	    
+	    DealList.remove(0);
+	    assertEquals("Test if that deal arraylist size is 1?", 1, DealList.size());    
+	  }
+	  
+	  @Test
+	  public void testAddDeal() {
+	    // Test if Item list is not null but empty, so that can add a new item
+	    assertNotNull("Test if there is valid deal arraylist to add to", DealList);
+	    DealList.add(deal1);
+	    assertEquals("Test if that deal arraylist size is 1?", 1, DealList.size());
+	    DealList.add(deal2);
+	    assertEquals("Test if that deal arraylist size is 2?", 2, DealList.size());
+	    
+	    
+	  }
+  
   @After
   public void tearDown() throws Exception {
     bid1 = null;
     bid2 = null;
     BidList = null;
+    deal1 = null;
+    deal2 = null;
+    DealList = null;
   }
   
     
