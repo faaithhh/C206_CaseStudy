a38cfa696f952920e0e9ef041e118c6edec11cfd
Cindy <ELJ@DESKTOP-2TN11OM>
2021-08-04 17:34:08 +0800

    edited

--- C206_CaseStudy/src/Account.java
+++ C206_CaseStudy/src/Account.java
@@ -26,19 +26,4 @@ public class Account {
 		return password;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public void setRole(String role) {
-		this.role = role;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
 }
